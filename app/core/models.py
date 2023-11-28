f6cb6e9f49d0e92b49e345650bb7e4beca17fed2
Chong Cheng Yang, Lionel <chonglionel97@gmail.com>
2023-11-28 23:28:55 +0800

    Add new custom user model

--- app/core/models.py
+++ app/core/models.py
@@ -1,3 +1,43 @@
-from django.db import models # noqa
+"""
+Database models
+"""
+from django.db import models
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    BaseUserManager,
+    PermissionsMixin
+)
 
-# Create your models here.
+
+class UserManager(BaseUserManager):
+    """Manager for user profiles"""
+
+    def create_user(self, email, password=None, **extra_fields):
+        """Create, save and return a new user profile"""
+        if not email:
+            raise ValueError('User must have an email address')
+        user = self.model(email=self.normalize_email(email), **extra_fields)
+        user.set_password(password)  # encrypts the password
+        # standard procedure for saving objects in django
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password):
+        """Create and save a new superuser with given details"""
+        user = self.create_user(email=email, password=password)
+        user.is_superuser = True  # is_superuser is created by PermissionsMixin
+        user.is_staff = True
+        user.save(using=self._db)
+        return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    """User in the system"""
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserManager()  # this is required for django to work with our custom user model
+
+    USERNAME_FIELD = 'email'
